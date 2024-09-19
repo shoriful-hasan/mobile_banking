4c9cebd17820b3bc8f2cd4e4ada5c94a8479add2
shoriful-hasan <143151322+shoriful-hasan@users.noreply.github.com>
2024-09-19 11:04:51 +0600

    second commit done

---
+++ js/home.js
@@ -0,0 +1,1 @@
+console.log('home page are added');
--- js/login.js
+++ js/login.js
@@ -5,6 +5,16 @@ document.getElementById('btn-login')
         const ph_login = document.getElementById('ph-login').value;
         const pas_login = document.getElementById('pas-login').value;
         console.log(ph_login,pas_login);
+
+    if(ph_login === '01753639166' && pas_login === '2635'){
+
+       window.location.href = 'home.html'
+
+        console.log('you are the validate user');
         
+    }
+        else{
+            alert('did not match the phone and  password')
+        }
         
       })
