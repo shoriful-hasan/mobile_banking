4155e7dda97e3c326aa03fa5aea5458b082bbaed
shoriful-hasan <143151322+shoriful-hasan@users.noreply.github.com>
2024-09-19 11:25:47 +0600

    third time js file are added

--- js/home.js
+++ js/home.js
@@ -1,1 +1,13 @@
-console.log('home page are added');
+
+
+document.getElementById('add_mony_input')
+                .addEventListener('click',function(event){
+                    event.preventDefault();
+    const Amount_input = document.getElementById('Amount_input').value;
+
+    const pinNumber_input = document.getElementById('pinNumber_input').value;
+
+    console.log(Amount_input,pinNumber_input);
+    
+
+})
